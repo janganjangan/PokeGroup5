7e03d3721ceb3151de7d025c8e103c71477730ea
Victor <angvictor91@gmail.com>
2021-10-21 14:48:54 +0700

    fix style

--- src/screens/PokeballScreen.js
+++ src/screens/PokeballScreen.js
@@ -89,6 +89,7 @@ const styles = StyleSheet.create({
     container: {
         marginTop: 20,
         flex: 1,
+        paddingBottom: 40,
         paddingTop: Platform.OS === 'ios' ? '20' : 0
     },
     btnResultContainer: {
