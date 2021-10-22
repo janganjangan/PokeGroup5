ac0e4d46bbd5e59ee3f053a3ad554ad49d41e85e
Victor <angvictor91@gmail.com>
2021-10-22 15:58:16 +0700

    save pokemons with redux and redux persist, fix styling

--- src/screens/PokedexScreen.js
+++ src/screens/PokedexScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Text, StyleSheet, View, FlatList} from 'react-native';
+import { Text, StyleSheet, View, FlatList } from 'react-native';
 import { useRoute } from '@react-navigation/core';
 import PokeImage from '../components/PokeImage';
 import PokeProperties from '../components/PokeProperties';
+import { ErrorMessage } from '../components';
 
 // const data = [
 //   {
@@ -27,51 +28,65 @@ import PokeProperties from '../components/PokeProperties';
 //   }
 // ];
 
-const Item = ({ imgUri,idx }) => {
+const Item = ({ imgUri, idx }) => {
   return (
     <View style={css.list}>
-        <PokeImage imageUri= {imgUri}/>
-        <PokeProperties index={idx} />
+      <PokeImage imageUri={imgUri} />
+      <PokeProperties index={idx} />
     </View>
   );
 }
 
 const PokedexScreen = () => {
 
-    const route = useRoute();
-    
-    const renderItem = ({ item }) => (
-      <Item imgUri={item.imageUri} idx={item.id}/>
-    );
-   
+  const route = useRoute();
+
+  const renderItem = ({ item, index }) => (
+    <Item key={index} imgUri={item.imageUri} idx={item.id} />
+  );
+
+  const renderHeader = () => {
     return (
-        <View>
-            <View>
-              <Text style={css.header}>Poke Collections</Text>
-            </View>
-            <FlatList
-              data={route.params.listPokemonIds}
-              keyExtractor={({ id }, index) => id}
-              renderItem={renderItem}
-            />         
-        </View>
+      <View>
+        <Text style={[css.header, route.params.type === 'new' && css.smallHeader]}>
+          {route.params.type === 'new' ? `New Poke Collections` : `Poke Collections`}
+        </Text>
+      </View>
     )
+  }
+
+  if ((route.params.listPokemonIds || []).length == 0)
+    return <ErrorMessage message={'No Data'} />
+
+  return (
+    <View>
+      <FlatList
+        ListHeaderComponent={renderHeader}
+        data={route.params.listPokemonIds}
+        keyExtractor={({ id }, index) => id}
+        renderItem={renderItem}
+      />
+    </View>
+  )
 }
 
-const css=StyleSheet.create({
-  list:{
-    flexDirection:'row',
-    alignItems:'center', 
-    marginTop:20,
-    borderWidth: 3, 
+const css = StyleSheet.create({
+  list: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 20,
+    borderWidth: 3,
     borderColor: "blue"
   },
-  header:{
-    fontSize:25,
-    fontWeight:'bold',
-    letterSpacing:10,
-    marginTop:20,
-    color:'#a3842e'
+  header: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    letterSpacing: 10,
+    marginTop: 20,
+    color: '#a3842e'
+  },
+  smallHeader: {
+    fontSize: 20
   }
 })
 
